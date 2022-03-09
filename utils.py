53ff0ed40faa72ad60142230ef156d034e852a79
lftxd1 <1608698313@qq.com>
2022-03-09 13:16:06 +0800

    layer

---
+++ utils.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+import numpy as np
+import time,os,json,glob,shutil
+
+def log(text):
+    text=str(text)
+    dt=datetime.now()
+    logText=dt.strftime(" [%Y-%m-%d %H:%M:%S]: ")+text
+    with open('log.txt','a+',encoding="utf-8") as f:
+        f.write(logText+"\n")
+    print(logText)
+
+def readTxt(file):
+    with open(file,'r',encoding="utf-8") as f:
+        return f.read()
+
+
+def toArray(list):
+    return np.array(list)
+
+
+
+def threeSequecne(text):
+    t_list=[]
+    for i in range(len(text)-2):
+        t_list.append(text[i:i+3])
+    return " ".join(t_list)    
+
+def threeSequecneCnn(text):
+    t_list=[]
+    for i in range(len(text)-1):
+        t_list.append(text[i:i+2])
+    return toArray(t_list) 
+
+
+
+def anySequecne(text,value):
+    t_list=[]
+    for i in range(len(text)-(value-1)):
+        t_list.append(text[i:i+value])
+    return " ".join(t_list) 
