922e57c07a7518b26c95d4c75299a9c549664dd0
lftxd1 <1608698313@qq.com>
2022-05-27 04:57:15 +0800

    update

---
+++ benchmark_identifier.py
@@ -0,0 +1,219 @@
+from random import shuffle
+from turtle import shape
+from numpy import dtype
+import tensorflow as tf
+from sklearn.model_selection import KFold
+import sys
+sys.path.append('.')
+from utils.MyUtils import *
+
+   
+def readTxt(file):
+    with open(file,'r',encoding="utf-8") as f:
+        return f.read().split("\n")
+
+dic={'A':'A','T':'T','G':'G','C':'C'}
+dic_con={'A':1,'T':2,'G':3,'C':4}
+def readTxt(file):
+    with open(file,'r',encoding="utf-8") as f:
+        return f.read().split("\n")
+
+non_test=readTxt("datasets/non_test.txt")
+non_train=readTxt("datasets/non_train.txt")
+strong_test=readTxt("datasets/strong_test.txt")
+strong_train=readTxt("datasets/strong_train.txt")
+weak_test=readTxt("datasets/weak_test.txt")
+weak_train=readTxt("datasets/weak_train.txt")
+
+def GenerateFromTextToNumpy(label,train):
+    train_con=[]
+    train_text=[]
+    train_text_5=[]
+    train_y=[]
+    for i in train:
+        
+        con_t=[dic_con[key] for key in i]
+        train_con.append(np.array(con_t))
+
+        t=threeSequecne(i,4)  
+        train_text.append(np.array(t))
+
+        t=threeSequecne(i,7) 
+        train_text_5.append(np.array(t))
+
+        train_y.append(np.array([label]))
+    train_con=np.array(train_con)
+    train_text_5=np.array(train_text_5)
+    train_text=np.array(train_text)
+    train_y=np.array(train_y)
+    return (train_con,train_text,train_y,train_text_5)
+
+def GenerateLayerOneTestData():
+    non_test_data=GenerateFromTextToNumpy(0,non_test)
+    strong_test_data=GenerateFromTextToNumpy(1,strong_test)
+    weak_test_data=GenerateFromTextToNumpy(1,weak_test)
+    test_x={"con":np.concatenate((non_test_data[0],strong_test_data[0],weak_test_data[0]))[:,:,np.newaxis],"text":np.concatenate((non_test_data[1],strong_test_data[1],weak_test_data[1])),"text_5":np.concatenate((non_test_data[3],strong_test_data[3],weak_test_data[3]))}
+    test_y=np.concatenate((non_test_data[2],strong_test_data[2],weak_test_data[2]))
+    return test_x,test_y
+
+def returnAccuracy():
+    import math
+    TP=self_evaluate(po_test,test_y[0:200])
+    TN=self_evaluate(ne_test,test_y[200:])
+    FP=1-TN
+    FN=1-TP
+    SN=TP/(TP+FN)
+    SP=TN/(TN+FP)
+    ACC=(TP+TN)/(TP+TN+FN+FP)
+    try:
+        MCC=(TP*TN-FP*FN)/math.sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN))
+    except:
+        MCC=-1
+    text={"ACC":ACC,"SP":SP,"SN":SN,"MCC":MCC}
+    return text
+def self_evaluate(x,y):
+    res=tf.nn.sigmoid(model.predict(x))  
+    res=np.array(res)
+    res[res>=0.5]=1
+    res[res<0.5]=0
+    sum=0
+    correct=0
+    for i,j in zip(res,y):
+        sum+=1
+        if i==j:
+            correct+=1
+    return correct/sum
+non_train_data=GenerateFromTextToNumpy(0,non_train)
+strong_train_data=GenerateFromTextToNumpy(1,strong_train)
+weak_train_data=GenerateFromTextToNumpy(1,weak_train)
+
+train_text=np.concatenate((non_train_data[1],strong_train_data[1],weak_train_data[1]))
+train_con=np.concatenate((non_train_data[0],strong_train_data[0],weak_train_data[0]))[:,:,np.newaxis]
+train_text_5=np.concatenate((non_train_data[3],strong_train_data[3],weak_train_data[3]))
+
+train_y=np.concatenate((non_train_data[2],strong_train_data[2],weak_train_data[2]))
+test_x,test_y=GenerateLayerOneTestData()
+
+po_test=test_x.copy()
+po_test["con"]=po_test["con"][0:200]
+po_test["text"]=po_test["text"][0:200]
+po_test["text_5"]=po_test["text"][0:200]
+ne_test=test_x.copy()
+ne_test["con"]=ne_test["con"][200:]
+ne_test["text"]=ne_test["text"][200:]
+ne_test["text_5"]=ne_test["text_5"][200:]
+
+
+kf=KFold(n_splits=10,shuffle=True,random_state=5)
+t=kf.split(train_text)
+index_list=[(i[0],i[1]) for i in t]
+
+
+def load_model():
+        global encoder
+        kernel_num=256
+        kernel_size_1=1
+        kernel_size_2=2
+        kernel_size_3=3
+        input_con=tf.keras.Input(shape=(200,1),name='con')
+        y=tf.keras.layers.Conv1D(kernel_num,kernel_size=kernel_size_1,strides=1,padding='same', activation=tf.nn.relu,kernel_regularizer=tf.keras.regularizers.l2(0.01))(input_con)
+        y=tf.keras.layers.Conv1D(kernel_num, kernel_size=kernel_size_1,strides=1, padding='same', activation=tf.nn.relu,kernel_regularizer=tf.keras.regularizers.l2(0.01))(y)
+        
+        block1_output=tf.keras.layers.BatchNormalization()(y)
+
+        y=tf.keras.layers.Conv1D(kernel_num, kernel_size=kernel_size_2,strides=1, padding='same', activation=tf.nn.relu,kernel_regularizer=tf.keras.regularizers.l2(0.01))(block1_output)
+        y=tf.keras.layers.Conv1D(kernel_num, kernel_size=kernel_size_2,strides=1, padding='same', activation=tf.nn.relu,kernel_regularizer=tf.keras.regularizers.l2(0.01))(y)
+
+        y=tf.keras.layers.BatchNormalization()(y)
+        
+        block2_output=tf.keras.layers.add([y,block1_output])
+
+        y=tf.keras.layers.Conv1D(kernel_num, kernel_size=kernel_size_3, padding='same', activation=tf.nn.relu,kernel_regularizer=tf.keras.regularizers.l2(0.01))(block2_output)
+        y=tf.keras.layers.Conv1D(kernel_num, kernel_size=kernel_size_3, padding='same', activation=tf.nn.relu,kernel_regularizer=tf.keras.regularizers.l2(0.01))(y)
+        y=tf.keras.layers.BatchNormalization()(y)
+        
+        block3_output=tf.keras.layers.add([y,block2_output])
+        
+
+        y=tf.keras.layers.Conv1D(kernel_num, kernel_size=3, padding='same', activation=tf.nn.relu,kernel_regularizer=tf.keras.regularizers.l2(0.01))(block3_output)
+
+        y=tf.keras.layers.GlobalAveragePooling1D()(y)
+        y=tf.keras.layers.Dense(256,activation='relu')(y)
+ 
+        input_text=tf.keras.Input(shape=(1,),dtype='string',name="text")
+        encoder=tf.keras.layers.TextVectorization(max_tokens=100)
+        encoder.adapt(train_text)
+        x=encoder(input_text)
+
+        x=tf.keras.layers.Embedding(input_dim=len(encoder.get_vocabulary()),output_dim=64,mask_zero=True)(x)
+        x=tf.keras.layers.Attention(name="ATT12")([x,x])
+        x=tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128,return_sequences=True,name="LSTM12"),name="B11")(x)
+        x=tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128,name="LSTM21"),name="B12")(x)
+        x=tf.keras.layers.Dense(512,activation='relu')(x)
+
+        input_text_5=tf.keras.Input(shape=(1,),dtype='string',name="text_5")
+        encoder_5=tf.keras.layers.TextVectorization(max_tokens=100)
+        encoder_5.adapt(train_text_5)
+        z=encoder_5(input_text_5)
+
+        z=tf.keras.layers.Embedding(input_dim=len(encoder.get_vocabulary()),output_dim=64,mask_zero=True)(z)
+        z=tf.keras.layers.Attention(name="ATT1")([z,z])
+        z=tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128,return_sequences=True,name="LSTM1"),name="B1")(z)
+        z=tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128,name="LSTM2"),name="B2")(z)
+        z=tf.keras.layers.Dense(512,activation='relu')(z)
+
+        x=tf.keras.layers.add([z,x])
+
+        feature_layer=tf.keras.layers.concatenate([x,y])
+
+        att=tf.keras.layers.Attention()([feature_layer,feature_layer,feature_layer])
+
+        d=tf.keras.layers.Dense(768,activation='relu')(att)
+        d=tf.keras.layers.Dropout(0.1)(d)
+        output=tf.keras.layers.Dense(1)(d)
+        model=tf.keras.Model([input_text_5,input_text,input_con],output)
+        base_learning_rate = 0.001
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
+                loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),metrics=['accuracy'])
+        return model
+
+def train(index):
+    epochs=120
+    TRAIN_ACC=0
+    MAX_ACC=0
+    loss=100
+    text={}
+    val_acc=0
+    for i in range(epochs):
+        if i<30:
+            lr=0.001
+        elif i<80:
+            lr=0.0005
+        else:
+            lr=0.0001
+        def scheduler(epoch):
+            return lr
+        reduce_lr = tf.keras.callbacks.LearningRateScheduler(scheduler)
+        print(f"index:{index},epochs:{i}")
+        history=model.fit(each_fold_train_x,each_fold_train_y,verbose = 0,shuffle=True,epochs=1,validation_data=(each_fold_val_x,each_fold_val_y),batch_size=128,callbacks=[reduce_lr])
+        if history.history["val_loss"][0]<loss:
+            loss=history.history["val_loss"][0]
+            text=returnAccuracy()
+            text["val_acc"]=self_evaluate(each_fold_val_x,each_fold_val_y)
+        #print(text,history.history)
+        print(self_evaluate(each_fold_train_x,each_fold_train_y))
+    record.append(text) 
+
+
+if __name__=="__main__":
+    record=[]
+    for i in range(0,10):
+        fold_order=i
+        (train_index,val_index)=index_list[fold_order]
+        each_fold_train_x={"con":train_con[train_index],"text":train_text[train_index],"text_5":train_text_5[train_index]}
+        each_fold_train_y=train_y[train_index]
+        each_fold_val_x={"con":train_con[val_index],"text":train_text[val_index],"text_5":train_text_5[val_index]}
+        each_fold_val_y=train_y[val_index]
+        model=load_model()
+        train(i)
+    print(record)
---
+++ readme.md
@@ -0,0 +1,62 @@
+## Indroduction
+
+Welcome to the iEnhancer-RLA code repository. iEnhancer-RLA is a deep learning method based on a self-attentive mechanism to fuse local and global features of sequences to identify and classify enhancers.
+
+
+
+## Performance
+
+The experimental results on the independent test dataset indicate that iEnhancer-RLA performs better than nine existing state-of-the-art methods in both identification and classification of enhancers in all metrics.
+
+**Enhancer identifier :** 
+
+| Method           | ACC (%)      | SP (%)       | SN (%)       | MCC          |
+| ---------------- | ------------ | ------------ | ------------ | ------------ |
+| EnhancerPred     | $74.00$      | $74.50$      | $73.50$      | $0.480$      |
+| iEnhancer-2L     | $73.00$      | $75.00$      | $71.00$      | $0.460$      |
+| iEnhancer-EL     | $74.75$      | $78.50$      | $71.00$      | $0.496$      |
+| iEnhancer-5Step  | $82.30$      | $83.50$      | $81.10$      | $0.650$      |
+| iEnhancer-ECNN   | $76.90$      | $75.20$      | $78.50$      | $0.537$      |
+| iEnhancer-XG     | $75.75$      | $77.50$      | $74.50$      | $0.515$      |
+| iEnhancer-EBLSTM | $77.20$      | $79.50$      | $75.50$      | $0.272$      |
+| iEnhancer-GAN    | $78.40$      | $75.80$      | $81.10$      | $0.567$      |
+| iEnhancer-RD     | $78.80$      | $76.50$      | $81.00$      | $0.576$      |
+| iEnhancer-RLA    | $\bf{93.72}$ | $\bf{90.45}$ | $\bf{97.00}$ | $\bf{0.876}$ |
+| Improvement      | +$13.8$%     | +$8.3$%      | +$19.7$%     | +$34.7$%     |
+
+**Enhancer classifier :** 
+
+| Method           | ACC (%)      | SP (%)  | SN (%)       | MCC          |
+| ---------------- | ------------ | ------- | ------------ | ------------ |
+| EnhancerPred     | $55.00$      | $65.00$ | $45.00$      | $0.102$      |
+| iEnhancer-2L     | $60.50$      | $74.00$ | $47.00$      | $0.218$      |
+| iEnhancer-EL     | $61.00$      | $68.00$ | $54.00$      | $0.222$      |
+| iEnhancer-5Step  | $63.50$      | $74.00$ | $53.00$      | $0.280$      |
+| iEnhancer-ECNN   | $67.80$      | $56.40$ | $79.10$      | $0.368$      |
+| iEnhancer-XG     | $63.50$      | $57.00$ | $70.00$      | $0.272$      |
+| iEnhancer-EBLSTM | $65.80$      | $53.60$ | $81.20$      | $0.324$      |
+| iEnhancer-GAN    | $74.90$      | $53.70$ | $96.10$      | $0.505$      |
+| iEnhancer-RD     | $70.50$      | $57.00$ | $84.00$      | $0.426$      |
+| iEnhancer-RLA    | $\bf{84.40}$ | $70.00$ | $\bf{98.80}$ | $\bf{0.718}$ |
+| Improvement      | +12.6%       | -5.4%   | +2.8%        | +42.1%       |
+
+
+
+## Environment requirements
+
+1. Python 3.7+
+2. Tensorflow 2.0+
+
+
+
+## Usage
+
+```bash
+git clone https://github.com/lftxd1/iEnhancer-RLA.git
+cd iEnhancer-RLA
+pip install -r requirements.txt
+python benchmark_identifier.py
+python benchmark_classifier.py
+python rice_identifier.py 
+```
+
